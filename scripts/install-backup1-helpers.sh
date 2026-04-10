4cc7c09c8d3d864a2f09b812bbcae24804a9e5f7
Alejandro <Bludi90@users.noreply.github.com>
2026-04-10 15:34:16 +0200

    dr: add backup1 system export helper

--- scripts/install-backup1-helpers.sh
+++ scripts/install-backup1-helpers.sh
@@ -7,13 +7,23 @@ SUDOERS_DIR="$SRC_DIR/sudoers"
 
 install -o root -g root -m 0750 "$SRC_DIR/backup1-safe-poweroff" /usr/local/sbin/backup1-safe-poweroff
 install -o root -g root -m 0750 "$SRC_DIR/backup1-zfs-restore-helper" /usr/local/sbin/backup1-zfs-restore-helper
+install -o root -g root -m 0750 "$SRC_DIR/backup1-system-export-helper" /usr/local/sbin/backup1-system-export-helper
 
 install -o root -g root -m 0440 "$SUDOERS_DIR/alejandro-backup1-poweroff" /etc/sudoers.d/alejandro-backup1-poweroff
 install -o root -g root -m 0440 "$SUDOERS_DIR/alejandro-backup1-zfs-restore-helper" /etc/sudoers.d/alejandro-backup1-zfs-restore-helper
+install -o root -g root -m 0440 "$SUDOERS_DIR/alejandro-backup1-system-export-helper" /etc/sudoers.d/alejandro-backup1-system-export-helper
 
 visudo -cf /etc/sudoers.d/alejandro-backup1-poweroff
 visudo -cf /etc/sudoers.d/alejandro-backup1-zfs-restore-helper
+visudo -cf /etc/sudoers.d/alejandro-backup1-system-export-helper
 
 echo "OK: backup1 helpers instalados"
-ls -l /usr/local/sbin/backup1-safe-poweroff /usr/local/sbin/backup1-zfs-restore-helper
-ls -l /etc/sudoers.d/alejandro-backup1-poweroff /etc/sudoers.d/alejandro-backup1-zfs-restore-helper
+ls -l \
+  /usr/local/sbin/backup1-safe-poweroff \
+  /usr/local/sbin/backup1-zfs-restore-helper \
+  /usr/local/sbin/backup1-system-export-helper
+
+ls -l \
+  /etc/sudoers.d/alejandro-backup1-poweroff \
+  /etc/sudoers.d/alejandro-backup1-zfs-restore-helper \
+  /etc/sudoers.d/alejandro-backup1-system-export-helper
